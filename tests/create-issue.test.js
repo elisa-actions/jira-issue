1b37b944f5bb233321cdc69bd778892b383226ea
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2021-02-04 09:48:07 +0200

    fix: add more cleanup to handle Dependabot output

--- tests/create-issue.test.js
+++ tests/create-issue.test.js
@@ -70,11 +70,27 @@ test("issue body is cleaned up", async () => {
       "Ticket description\n" +
       "<details><summary><strong>Bug fixes</strong></summary><p>\n" +
       "- some fix [abcd12](link)\n" +
-      "</p></details>",
+      "</p></details>\n" +
+      "<em>Emphasis</em>\n" +
+      "<ul>\n<li>list item</li>\n</ul>\n" +
+      '<a href="http://example.com">link</a>\n' +
+      "<h1>Title</h1>\n" +
+      "![Dependabot compatibility score|https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=package&package-manager=pip&previous-version=2.1.0&new-version=2.2.0]](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)",
   });
   await createIssue();
   expect(newIssue).toHaveBeenCalledWith(
     "Ticket title",
-    "Ticket description\n*Bug fixes*\n- some fix [abcd12|link]\n\n*Author*\n[~user1@example.com]\n\n*Reviewers*\n[user2|https://github.com/user2]\n"
+    "Ticket description\n" +
+      "*Bug fixes*\n" +
+      "- some fix [abcd12|link]\n\n" +
+      "_Emphasis_\n\n" +
+      "- list item\n\n" +
+      "[link|http://example.com]\n" +
+      ".h1 Title\n" +
+      "!https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=package&package-manager=pip&previous-version=2.1.0&new-version=2.2.0!\n\n" +
+      "*Author*\n" +
+      "[~user1@example.com]\n\n" +
+      "*Reviewers*\n" +
+      "[user2|https://github.com/user2]\n"
   );
 });
