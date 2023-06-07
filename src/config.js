8316de92e34378a951dc878b4fda263142eef690
Timo Hakkarainen <timo.hakkarainen@elisa.fi>
2023-06-07 15:09:52 +0300

    fix(deps): safeLoad is deprecated

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ const yaml = require("js-yaml");
 
 exports.parseConfig = function () {
   const configFile = core.getInput("configuration-file");
-  const config = yaml.safeLoad(fs.readFileSync(configFile, "utf-8"));
+  const config = yaml.load(fs.readFileSync(configFile, "utf-8"));
   return config;
 };
